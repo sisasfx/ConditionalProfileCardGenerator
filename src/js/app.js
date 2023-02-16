aaf50e989ad63d734c63eb3796890f46736ce749
Miquel Ortiz Graupera <sisasfx@gmail.com>
2023-02-16 19:05:34 +0000

    feat/ all done

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,39 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>"${
+            variables.name
+              ? variables.name + " " + variables.lastname
+              : "Escribe tu nombre"
+          }"</h1>
+          <h2>${variables.role ? variables.role : "Escribe tu role"}</h2>
+          <h3>${variables.country ? variables.country : "Escribe tu pais"}</h3>
+          <h3>${variables.city ? variables.city : "Escribe tu ciudad"}</h3>
+          <ul class=${
+            variables.socialMediaPosition
+              ? variables.socialMediaPosition
+              : "position-right"
+          }>
+            <li><a href=${
+              variables.twitter
+                ? variables.twitter
+                : "https://twitter.com/4geeksacademy"
+            }><i class="fab fa-twitter"></i></a></li>
+            <li><a href=${
+              variables.github
+                ? variables.github
+                : "https://github.com/4geeksacademy"
+            }><i class="fab fa-github"></i></a></li>
+            <li><a href=${
+              variables.linkedin
+                ? variables.linkedin
+                : "https://linkedin.com/4geeksacademy"
+            }><i class="fab fa-linkedin"></i></a></li>
+            <li><a href=${
+              variables.instagram
+                ? variables.instagram
+                : "https://instagram.com/4geeksacademy"
+            }><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
